3a4c307618430b3452a1e1bdebf28e818c2fe301
Adarsh Medikonda <medikonda.adarsh@gmail.com>
2020-09-09 21:22:08 -0500

    BIG UPDATE WITH ANIMATIONS

---
+++ js/contact.js
@@ -0,0 +1,7 @@
+document.getElementById("github").innerHTML = "github";
+document.getElementById("linkedin").innerHTML = "linkedin";
+
+anime({
+	targets: ['div.github', 'div.linkedin'],
+	translateX: 1250
+});
--- js/main.js
+++ js/main.js
@@ -1,9 +1,50 @@
-document.getElementById("resume").innerHTML = "résumé";
+/*
+ _     _        _          _  _
+| |_  (_)      | |_   ___ | || | ___
+|   \ | |      |   \ / -_)| || |/ _ \
+|_||_||_|      |_||_|\___||_||_|\___/
+*/
+
+document.getElementById("resume").innerHTML = "resume";
 document.getElementById("contact").innerHTML = "contact";
 document.getElementById("other").innerHTML = "other";
 
+var textSelection = document.getElementById("text-selection");
+textSelection.style.display = 'none';
+var body = document.getElementById("allBody");
+
+anime({
+	targets: 'section',
+	translateY: 100,
+});
+
+window.addEventListener('load', function(){
+	textSelection.style.display = 'flex';
+	anime({
+  	targets: ['div.resume', 'div.contact', 'div.other'],
+  	translateY: 100,
+  	delay: anime.stagger(50)
+	});	
+});
+
+function mouseHover(x) {
+  anime({
+    targets: x,
+    scale: 1.05
+  });
+}
+
+function mouseLeave(x) {
+  anime({
+    targets: x,
+    scale: 1
+  });
+}
+
+/*
 anime({
   targets: ['div.resume', 'div.contact', 'div.other'],
   translateY: 100,
   delay: anime.stagger(50)
 });
+*/
